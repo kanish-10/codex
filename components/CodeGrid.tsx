ca79bbc3149d20e87d3364c46d9306bc510b6572
Kanish Chheda <kanishchheda@gmail.com>
2024-03-21 21:19:10 +0530

    ...

--- components/CodeGrid.tsx
+++ components/CodeGrid.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import CodeCard from "@/components/CodeCard";
 import CreateCodeButton from "@/components/CreateCodeButton";
+import { Id } from "@/convex/_generated/dataModel";
 
 interface CodeGridProps {
   type: "home" | "profile";
@@ -11,15 +12,27 @@ interface CodeGridProps {
 }
 
 const CodeGrid = ({ type, id }: CodeGridProps) => {
-  let posts;
+  let posts:
+    | {
+        _id: Id<"posts">;
+        _creationTime: number;
+        code?: string | undefined;
+        input?: string | undefined;
+        authorId: Id<"users">;
+        language: string;
+        title: string;
+      }[]
+    | undefined;
+
   // eslint-disable-next-line react-hooks/rules-of-hooks
   if (type === "home") posts = useQuery(api.posts.findAll);
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  if (type === "profile") {
+  else if (type === "profile") {
     // @ts-ignore
     // eslint-disable-next-line react-hooks/rules-of-hooks
     posts = useQuery(api.posts.findByAuthor, { authorId: id });
-  }
+  } else posts = undefined;
+
   return (
     <>
       <div className="mt-5 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -37,7 +50,7 @@ const CodeGrid = ({ type, id }: CodeGridProps) => {
             />
           ))}
       </div>
-      {!posts || posts?.length === 0 ? (
+      {posts?.length === 0 && type === "profile" ? (
         <div className="flex h-[500px] flex-col items-center justify-center">
           <>
             <p>No Post made yet.</p>
